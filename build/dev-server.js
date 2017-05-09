ea7d5ad5974a21ce321d7de22401433014206e32
liuaajiang <1393571051@qq.com>
2017-05-09 21:26:48 +0800

    修改主机名

--- build/dev-server.js
+++ build/dev-server.js
@@ -62,7 +62,7 @@ app.use(hotMiddleware)
 var staticPath = path.posix.join(config.dev.assetsPublicPath, config.dev.assetsSubDirectory)
 app.use(staticPath, express.static('./static'))
 
-var uri = 'http://localhost:' + port
+var uri = '10.17.152.5:' + port
 
 var _resolve
 var readyPromise = new Promise(resolve => {
